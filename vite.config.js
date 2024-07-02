35db1e06c5601d927608998f9120d7e95a4d4de4
Martin <caceresmartin22@gmail.com>
2024-07-02 14:53:21 +0200

    V2.8.1
    Univer plugin for vite error

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from "url";
 import vue from '@vitejs/plugin-vue'
+import { univerPlugin } from "@univerjs/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), univerPlugin()],
   define: { 'process.env': {} },
   resolve: {
     alias: [
