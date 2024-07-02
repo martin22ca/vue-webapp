05d5688b5bf479d6318236df5d9a5d0e89ea9d4b
Martin <caceresmartin22@gmail.com>
2024-07-02 15:52:12 +0200

    V2.8.2
    Axios api error set to dev

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -5,7 +5,7 @@ const DEV_BASE_URL = 'http://127.0.0.1:8002/';
 
 const createAxiosClient = () => {
     return axios.create({
-        baseURL: DEV_BASE_URL,
+        baseURL: API_BASE_URL,
     });
 }
 export const axiosClient = createAxiosClient();
